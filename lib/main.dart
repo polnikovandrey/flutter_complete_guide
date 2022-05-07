8433a66374d30908f06ee6664c709dd0f59bf382
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-05-07 20:05:54 +0300

    App from scratch ..1

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: const Text('This is my default text'),
+      ),
+    );
   }
 }
